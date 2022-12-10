a534c04001c20fd45265b151fea4e96afa2a8759
BogartDME <edwin.pietrowski@gmail.com>
2022-12-09 21:42:22 -0500

    README file is generating

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name:"name",
+            message:"What is your name",
+        },
+        {
+            type: "input",
+            name:"email",
+            message:"What is your email?",
+        },
+        {
+            type: "input",
+            name:"github",
+            message:"What is your github user name?",
+        },
+        {
+            type: "input",
+            name:"title",
+            message:"What is the name of your project?",
+        },
+        {
+            type: "input",
+            name:"project description",
+            message:"Describe your project:",
+        },
+        {
+            type: "input",
+            name:"languages",
+            message:"How can this app be used?",
+        },
+        {
+            type: "input",
+            name:"requirements",
+            message:"What are your instructions for installation?",
+        },
+        {
+            type: "checkbox",
+            name:"languages",
+            message:"What languages did you use to build your app?",
+            choices:["CSS", "HTML", "Javascript", "React", "Java", "SQL", "Python", "C++", "Ruby", "Swift", "PHP"]
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "What license did you use?",
+            choices: ["MIT License", "Apache License v2.0", "GNU General Public License v3.0", "CZ Zero License", "Unilicense", "Unlicensed"],
+        },
+        {
+            type: "input",
+            name:"usage",
+            message:"How can this app be used?",
+        },
+        {
+            type: "input",
+            name:"contributors",
+            message:"Who contributed to the app?",
+        },
+        {
+            type: "input",
+            name:"tests",
+            message:"Instructions to run tests on the app:",
+        },
+        {
+            type: "input",
+            name:"Usage",
+            message:"How can this app be used?",
+        }
+    ])
+    .then((data) => {
+        writeToFile(data);
+    })
+};    
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+fs.writeFile('./dist/README.md', generateMarkdown(data), (err) =>
+err ? console.error(err) : console.log('Readme file created successfuly!'));
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) 
+  switch(license){
+    case "MIT License"
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+    case "Apache License v2.0"
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+
+
+
+
+  }
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
